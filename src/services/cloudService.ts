3ca2437cc01073ca292c99c2e87cd01d7e1579c5
Angel <angelo_stoyanov@abv.bg>
2024-03-19 15:52:19 +0200

    select all db images

--- src/services/cloudService.ts
+++ src/services/cloudService.ts
@@ -11,32 +11,19 @@ export class CloudRepository implements ICloudImages<CloudImages> {
     getAllImagesFromDB(): Promise<string[]> {
         return new Promise((resolve, reject) => {
             this.pool.query(`
-            SELECT distinct
-            trips.imageFile AS tripImageFile,
-            points.imageFile AS pointImageFile,
-            users.imageFile AS userImageFile
-        FROM
-            hack_trip.trips
-        LEFT JOIN
-            hack_trip.points ON trips._id = points._ownerTripId
-        LEFT JOIN
-            hack_trip.users ON trips._ownerId = users._id
-            WHERE
-            trips.imageFile IS NOT NULL AND trips.imageFile != '' AND
-            points.imageFile IS NOT NULL AND points.imageFile != '' AND
-            users.imageFile IS NOT NULL AND users.imageFile != '';
+            SELECT imageFile FROM hack_trip.trips WHERE imageFile IS NOT NULL
+            UNION
+            SELECT imageFile FROM hack_trip.points WHERE imageFile IS NOT NULL
+            UNION
+            SELECT imageFile FROM hack_trip.users WHERE imageFile IS NOT NULL;
             `, (err, rows, fields) => {
                 if (err) {
                     console.log(err);
                     reject(err);
                     return;
                 }
-
-              
                 const imageFiles = [
-                    ...rows.map(row => row.tripImageFile).filter(file => file && file.trim() !== '').map(file => file.split(/[,\s]+/)),
-                    ...rows.map(row => row.pointImageFile).filter(file => file && file.trim() !== '').map(file => file.split(/[,\s]+/)),
-                    ...rows.map(row => row.userImageFile).filter(file => file && file.trim() !== '').map(file => file.split(/[,\s]+/)),
+                    ...rows.map(row => row.imageFile).filter(file => file && file.trim() !== '').map(file => file.split(/[,\s]+/)),
                 ].filter(files => files.length > 0);
 
                 const mergedImageFiles = [].concat(...imageFiles);
